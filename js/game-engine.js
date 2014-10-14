870fe19e5c18b959157daf7bae762bf448d51d30
Sunny Li <sun.li@mail.utoronto.ca>
2014-10-14 12:22:08 -0400

    fix small markup issue
    change indentation

--- js/game-engine.js
+++ js/game-engine.js
@@ -1,6 +1,7 @@
 // We should probably check for canvas support first...
 
 // Canvas parameters
+var gameCanvas;
 var context;
 var canvasHeight = 500;
 var canvasWidth  = 800;
@@ -12,7 +13,7 @@ var ballY = 200;
 //Player start dimensions
 var playerHeight = 30;
 var playerWidth  = 150;
-var playerX = canvasWidth/2 - playerWidth/2;
+var playerX = canvasWidth / 2 - playerWidth / 2;
 var playerY = canvasHeight - playerHeight;
 
 // Movement speed
@@ -24,64 +25,67 @@ var currentLevel = 1;
 
 // Start the game
 function init() {
+  gameCanvas = document.getElementById("gameCanvas");
   context = gameCanvas.getContext('2d');
+  gameCanvas.height = canvasHeight;
+  gameCanvas.width = canvasWidth;
   setInterval(draw, 10);
 }
 
 // Update the canvas
 function draw() {
-    context.clearRect(0, 0, canvasWidth, canvasHeight);
-	context.beginPath();
-    
-    // Draw game objects
-	drawBall();
-	drawPlayer(); // TODO
-	
-	context.closePath();
+  context.clearRect(0, 0, canvasWidth, canvasHeight);
+  context.beginPath();
 
-    // Update game objects
-    updateBallAcceleration(dx, dy, 0);
-	checkCollision(ballX, ballY);
-	updateBallPosition();
-	updatePlayerPosition(); //TODO
+  // Draw game objects
+  drawBall();
+  drawPlayer(); // TODO
+
+  context.closePath();
+
+  // Update game objects
+  updateBallAcceleration(dx, dy, 0);
+  checkCollision(ballX, ballY);
+  updateBallPosition();
+  updatePlayerPosition(); //TODO
 }
 
 /********* Helper functions *********/
 
-function updatePlayerPosition(){
-	// TODO
+function updatePlayerPosition() {
+  // TODO
 }
 
 function drawPlayer() {
-	// TODO: colour this
-	context.fillRect(
-		playerX, 
-		playerY, 
-		playerWidth, 
-		playerHeight
-	);
+  // TODO: colour this
+  context.fillRect(
+    playerX,
+    playerY,
+    playerWidth,
+    playerHeight
+  );
 }
 
 function drawBall() {
-	context.fillStyle = "#00c39c";
-    context.arc(ballX, ballY, 20, 0, Math.PI*2, true);
-    context.fill();
+  context.fillStyle = "#00c39c";
+  context.arc(ballX, ballY, 20, 0, Math.PI * 2, true);
+  context.fill();
 }
 
 /* Update the ball's position. */
-function updateBallPosition(){
-	ballX += dx; 
-	ballY += dy;
+function updateBallPosition() {
+  ballX += dx;
+  ballY += dy;
 }
 
 /* Keep the ball within the canvas dimensions. */
 function checkCollision(ballX, ballY) {
-	if (ballX < 0 || ballX > canvasWidth) {
-		dx =- dx;
-	}; 
-	if (ballY < 0 || ballY > canvasHeight) {
-		dy =- dy;
-	};
+  if (ballX < 0 || ballX > canvasWidth) {
+    dx *= -1;
+  }
+  if (ballY < 0 || ballY > canvasHeight) {
+    dy *= -1;
+  }
 }
 
 /* Needed for second level, when acceleration increases */
